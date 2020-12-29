96c297bc325c7d600cba8bd722d9b88d47161e1d
22944855 <r.p.renken@web.de>
2020-12-29 01:29:29 +0100

    removed debugging features :see_no_evil:

--- packages/faithix/scripts/garage.js
+++ packages/faithix/scripts/garage.js
@@ -2,7 +2,6 @@ const garage_config = require("../garage_config.json")
 const database = require("../corestuff/mysql").getPool();
 
 function entercolshapehandler(player,shape) {
-    console.log(player.name + " entered colshape " + shape.id);
     player.setVariable("shapedata", shape);
 }
 
@@ -14,21 +13,12 @@ mp.events.add("playerEnterColshape", entercolshapehandler);
 mp.events.add("playerExitColshape", leavecolshapehandler);
 
 
-//Colshape loader
+//Colshape loader from mysql
 database.query("SELECT * from colshapes", (error, result) => {
     if(error) throw error;
-
-    console.log(result);
-    console.log(result.length);
     
     for(let i = 0; i < result.length; i++){
         var str = "const colshape"+result[i].id+" = mp.colshapes.newSphere(" + result[i].x + ", " + result[i].y + ", " + result[i].z + ", " + result[i].size + ", " + result[i].dim + ")";
-        console.log(str);
-        try {
-            eval(str);
-            console.log("colshape " + result[i].id + " created");
-        } catch (error) {
-            console.log(error);
-        };
+        eval(str);
     };
 });
