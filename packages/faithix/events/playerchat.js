6e4834a892a17d573b65f21094976c74b24cd175
22944855 <r.p.renken@web.de>
2020-12-30 13:29:08 +0100

    playerchat ready for test :D

---
+++ packages/faithix/events/playerchat.js
@@ -0,0 +1,8 @@
+const teamdata = require("../team_config.json")
+
+let teamColor = player.currentTeam ? teamdata[player.currentTeam].chatcolor : "#FFFFFF";
+
+mp.events.add("playerChat", (player, message) => {
+    if(player.health <= 0) return;
+    mp.players.broadcast(`!{${teamColor}}${player.name}: !{#FFFFFF}${message}`);
+})
