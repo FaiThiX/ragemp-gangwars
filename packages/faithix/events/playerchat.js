51d5a324c654c3544341cc1134016137efe69674
22944855 <r.p.renken@web.de>
2020-12-30 17:47:44 +0100

    added playerchat and fix teamchatcolor

--- packages/faithix/events/playerchat.js
+++ packages/faithix/events/playerchat.js
@@ -1,8 +1,8 @@
 const teamdata = require("../team_config.json");
 
-let chatcolor = player.currentTeam ? teamdata[player.currentTeam].chatcolor : "#FFFFFF";
-
 mp.events.add("playerChat", (player, message) => {
+    let chatcolor = player.currentTeam ? teamdata[player.currentTeam].chatcolor : "#FFFFFF";
+    
     if(player.health <= 0) return;
     mp.players.broadcast(`!{${chatcolor}}${player.name}: !{#FFFFFF}${message}`);
 });
