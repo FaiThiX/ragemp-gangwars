b948de932471074886a8ad706c0da4476584b10f
Rene <r.p.renken@web.de>
2021-01-08 16:12:33 +0100

    maybe this will work lg... i dont know about scopes that much

--- client_packages/scripts/misc.js
+++ client_packages/scripts/misc.js
@@ -1,14 +1,25 @@
 var markers = 1;
+var loaded = false;
 mp.events.add("loadmarker:client", (element) => {
-    
-    let marker = "let marker" + element.id + " = mp.markers.new(0,new mp.Vector3("+ element.x + ", " + element.y + ", " + element.z + "),2,{color: [77, 216, 49, 255], visible: true, dimension: " + mp.players.local.dimension + "})"
-    eval(marker);
-    markers = markers += 1;
+    markerhandler(element,"load");
 });
 
 mp.events.add("removemarker:client", () => {
-    for(let i=1; i <= markers; i++){
-        let removemarker = "marker" + i + ".destroy()";
-        eval(removemarker);
-    }
+    element = null;
+    markerhandler(element,"unload");
 })
+
+function markerhandler(element, state){
+    if(state === "load" && loaded == false){
+        loaded = true;
+        let marker = "let marker" + element.id + " = mp.markers.new(0,new mp.Vector3("+ element.x + ", " + element.y + ", " + element.z + "),2,{color: [77, 216, 49, 255], visible: true, dimension: " + mp.players.local.dimension + "})"
+        eval(marker);
+    }
+    else if(state === "unload" && loaded == true){
+        loaded = false;
+        for(let i=1; i <= markers; i++){
+            let removemarker = "marker" + i + ".destroy()";
+            eval(removemarker);
+        };
+    };
+};
