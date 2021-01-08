a0f4a3fe5d9589b50750b0b6abd58da45e875a40
Rene <r.p.renken@web.de>
2021-01-09 00:37:13 +0100

    rework markershit thx to Wdoyle2

--- client_packages/scripts/misc.js
+++ client_packages/scripts/misc.js
@@ -1,25 +1,20 @@
-var markers = 1;
-var loaded = false;
+const markers = [];
+const player = mp.players.local;
+
 mp.events.add("loadmarker:client", (element) => {
-    markerhandler(element,"load");
+    markerhandler(element, "load");
 });
 
 mp.events.add("removemarker:client", () => {
-    element = null;
-    markerhandler(element,"unload");
+    markerhandler(null, "unload");
 })
 
-function markerhandler(element, state){
-    if(state === "load" && loaded == false){
-        loaded = true;
-        let marker = "let marker" + element.id + " = mp.markers.new(0,new mp.Vector3("+ element.x + ", " + element.y + ", " + element.z + "),2,{color: [77, 216, 49, 255], visible: true, dimension: " + mp.players.local.dimension + "})"
-        eval(marker);
+function markerhandler(element, state) {
+    if (state === "load") {
+        markers[element.id] = mp.markers.new(0, new mp.Vector3(element.x, element.y, element.z), 2,{color: [77, 216, 49, 255], visible: true, dimension: player.dimension});
+    } else if (state === "unload" && markers.length > 0) {
+        for(let i in markers) {
+            markers[i].destroy();
+        }
     }
-    else if(state === "unload" && loaded == true){
-        loaded = false;
-        for(let i=1; i <= markers; i++){
-            let removemarker = "marker" + i + ".destroy()";
-            eval(removemarker);
-        };
-    };
 };
