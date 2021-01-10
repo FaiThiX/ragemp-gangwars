2284ef77099e4e2dba585c17b73f1eaffd31c6b8
22944855 <r.p.renken@web.de>
2021-01-10 22:59:36 +0100

    delete unused file

--- packages/faithix/events/leaveevents.js
+++ packages/faithix/events/leaveevents.js
@@ -3,7 +3,6 @@ const database = require("../corestuff/mysql").getPool();
 mp.events.add("playerQuit", (player, exitType, reason) => {
     database.query("UPDATE users SET kills=?, deaths=? WHERE socialclub=?",[player.kills, player.deaths, player.socialClub], (error, result) => {
         if(error) throw error;
-
-        console.log("Saved Account for " + player.name);
     });
+    console.log("Saved Account for " + player.name);
 });
