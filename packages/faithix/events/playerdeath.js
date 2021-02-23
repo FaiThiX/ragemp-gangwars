ef81c749d70a7c4015a49bd8b82ce8267a2a5f0f
Rene <r.p.renken@web.de>
2021-02-23 22:55:56 +0100

    try fix crash

--- packages/faithix/events/playerdeath.js
+++ packages/faithix/events/playerdeath.js
@@ -1,57 +1,42 @@
 const teams = require("../team_config");
 
+function ffaspawn(player) {
+    mp.events.call("spawnffa:server", player);
+}
+
+function cayospawn (player){
+    player.spawn(new mp.Vector3(4840.571, -5174.425, 2.0));
+}
+
+function spawn(player) {
+    player.spawn(teams[player.currentTeam].Spawnpos);
+}
+
 mp.events.add("playerDeath", (player, reason, killer) => {
-    try {
-        if(mp.players.exists(player)){
-            if (player.respawnTimer) clearTimeout(player.respawnTimer);
+    
+    console.log("[DEATH] Player: " + player.name + " died because " + reason + " !")
 
-            if(player.cayo === true){
-                player.respawnTimer = setTimeout(() => {
-                    player.spawn(new mp.Vector3(4840.571, -5174.425, 2.0));
-                    clearTimeout(player.respawnTimer);
-                    player.respawnTimer = undefined;
-                    return;
-                }, 8000);
-            }
-            else if (player.isffa === 1){
-
-                if(killer && killer.id !== player.id){
-                    killer.health = 100;
-                    killer.armour = 100;
-                    player.deaths = player.deaths += 1;
-                    killer.kills = killer.kills += 1;
-                };
-                if (killer === undefined){
-                    player.deaths = player.deaths += 1;
-                };
-                player.respawnTimer = setTimeout(() => {
-                    mp.events.call("spawnffa:server", player);    
-                    clearTimeout(player.respawnTimer);
-                    player.respawnTimer = undefined;
-                    return;
-                }, 8000);
-            }
-            else {
-                player.respawnTimer = setTimeout(() => {
-                    console.log("[DEATH] Player: " + player.name + " died because " + reason + " !")
-                    if (killer === undefined){
-                        player.deaths = player.deaths += 1;
-                    };
-                    if (killer && killer.id !== player.id){
-                        player.deaths = player.deaths += 1;
-                        killer.kills = killer.kills += 1;
-                    };
-            
-                    player.spawn(teams[player.currentTeam].Spawnpos);
-                    player.dimension = 0;
-                    clearTimeout(player.respawnTimer);
-                    player.respawnTimer = undefined;
-                }, 8000);
-            
-            };
-        };
-    } catch (error) {
-        console.log(error)
+    if(killer && killer.id !== player.id){
+        player.deaths = player.deaths += 1;
+        killer.kills = killer.kills += 1;
+    };
+    
+    if(player.cayo === true){
+        setTimeout(() => {
+            cayospawn(player);
+        }, 8000);
+    }
+    else if(player.isffa === 1){
+        killer.health = 100;
+        killer.armour = 100;
+        setTimeout(() => {
+            ffaspawn(player);
+        }, 8000);
+    }
+    else{
+        setTimeout(() => {
+            spawn(player);
+        }, 8000);
     };
 });
 
